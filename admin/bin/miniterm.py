0562b37f1d47c91f29cf319b9bdbc854dcb02f4c
hendmzh <Hendmzh@hotmail.com>
2018-03-20 07:40:23 +0300

    Demo 1 final
    final edits after demo1

--- admin/bin/miniterm.py
+++
@@ -1,4 +0,0 @@
-#!/Users/hendalzahrani/adminSite/bin/python3.6
-# EASY-INSTALL-SCRIPT: 'pyserial==3.4','miniterm.py'
-__requires__ = 'pyserial==3.4'
-__import__('pkg_resources').run_script('pyserial==3.4', 'miniterm.py')
--- admin/static/tables.py
+++
@@ -1,7 +0,0 @@
-from flask_table import Table, Col
-
-class Results(Table):
-    id = Col('Id', show=False)
-    room = Col('Room')
-    name = Col('Name')
-    state = Col('State')
